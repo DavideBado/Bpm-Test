701bdc1e588dc3df2faf85920aede21456f54fca
DavideBado <dabado2510@gmail.com>
2019-01-28 19:50:55 +0100

    60Bpm sembra funzionare, 120 e 240 no, con loro lo SpawnTime inviato ai cloni risulta 0 quindi Speed = infinito che da ovviamente errore e cloni immobili.

---
+++ Assets/NotaMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotaMove : MonoBehaviour {
+    float Speed;
+    Collider collider;
+    float Space;
+    public float SpawnTime;
+	// Use this for initialization
+	void Start ()
+    {
+        collider = GetComponent<Collider>();
+        Space = collider.bounds.size.y;
+        Speed = (Space / SpawnTime) + 1; // +1 per separarle
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Muoviti();
+	}
+    void Muoviti()
+    {
+        if (tag == "Nota")
+        {
+            transform.position -= new Vector3(0, Speed * Time.deltaTime);
+        }
+
+    }
+}
---
+++ Assets/Spawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+    public int Bpm;
+    float SpawnTime;
+    GameObject Nota;
+	// Use this for initialization
+	void Start ()        
+    {
+        SpawnTime = 60 / Bpm;
+        Nota = GameObject.Find("Nota");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Spawnala();
+	}
+    void Spawnala()
+    {
+        SpawnTime -= Time.deltaTime;
+        if(SpawnTime <= 0)
+        {
+            GameObject Clone = GameObject.Instantiate(Nota, transform.position, Quaternion.identity);
+            Clone.GetComponent<NotaMove>().SpawnTime = 60 / Bpm;
+            Clone.tag = "Nota";
+            SpawnTime = 60 / Bpm;            
+        }
+    }
+}
